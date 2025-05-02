77e21a80ba7d52326c4a70d89eada9c934b7bc07
Egor Gusev <86432916+Thunderdew@users.noreply.github.com>
2025-05-02 14:34:07 +0300

    method for comparing repositories

--- plagiarism-app/src/main/java/com/example/plagiarismapp/service/RepositoryService.java
+++ plagiarism-app/src/main/java/com/example/plagiarismapp/service/RepositoryService.java
@@ -9,6 +9,9 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class RepositoryService {
@@ -21,5 +24,30 @@ public class RepositoryService {
                 .runOn(Schedulers.boundedElastic())
                 .flatMap(url -> Mono.fromCallable(() -> gitService.downloadRepository(url, request.getLanguage())))
                 .sequential();
+    }
+
+    public CompareResultDto comparingRepositories(RepositoryRequest request) {
+        List<RepositoryContent> contents = new ArrayList<>();
+        CompareResultDto compareResult = new CompareResultDto();
+        compareResult.setResults(new ArrayList<>());
+
+
+        for (String url : request.getRepositoryUrls()) {
+            try {
+                contents.add(gitService.downloadRepository(url, request.getLanguage()));
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        for (int i = 0; i < contents.size(); i++) {
+            for (int j = i + 1; j < contents.size(); j++) {
+                compareResult.getResults().add(coreService.compareRepositories(contents.get(i), contents.get(j)));
+            }
+        }
+
+        return compareResult;
     }
 }
--- plagiarism-core/src/main/java/org/example/service/CoreService.java
+++ plagiarism-core/src/main/java/org/example/service/CoreService.java
@@ -1,6 +1,14 @@
 package org.example.service;
 
+import com.example.content.FileContent;
+import com.example.content.RepositoryContent;
+import org.antlr.v4.runtime.misc.Pair;
 import org.example.token.strategy.TokenCollectorManager;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class CoreService {
     private final TokenCollectorManager tokenCollectorManager;
@@ -9,8 +17,74 @@ public class CoreService {
         this.tokenCollectorManager = tokenCollectorManager;
     }
 
-//    public Result findPlagiarism(List<RepositoryContent>) {
-//
-//    }
+    public CompareTwoRepositoryDto compareRepositories (RepositoryContent firstRepository, RepositoryContent secondRepository) {
+
+        CompareTwoRepositoryDto compareResult = new CompareTwoRepositoryDto();
+        compareResult.setFirstRepository(firstRepository);
+        compareResult.setSecondRepository(secondRepository);
+        compareResult.setCompareFiles(new ArrayList<>());
+
+        Set<Pair<FileContent, FileContent>> comparedFiles = new HashSet<>();
+
+        for (FileContent file : firstRepository.getFiles()) {
+            for (FileContent file2 : secondRepository.getFiles()) {
+                Pair<FileContent, FileContent> pair = new Pair(file, file2);
+                comparedFiles.add(pair);
+            }
+        }
+
+        for (Pair<FileContent, FileContent> pair : comparedFiles) {
+            CompareTwoFilesDto result = new CompareTwoFilesDto();
+            FileContent firstFile = pair.a;
+            FileContent secondFile = pair.b;
+
+
+            result.setFullFilenameFirst(firstFile.getFullFilename());
+            result.setFullFilenameSecond(secondFile.getFullFilename());
+
+
+
+            List<TokenInfo> Submission1Tokens = tokenCollectorManager.collectTokensFromFile(firstRepository.getLanguage(), firstFile.getContent());
+            List<TokenInfo> Submission2Tokens = tokenCollectorManager.collectTokensFromFile(secondRepository.getLanguage(), secondFile.getContent());
+
+
+            StringBuilder submissionBuild1 = new StringBuilder();
+            for (TokenInfo token : Submission1Tokens) {
+                submissionBuild1.append((char) token.type);
+            }
+            String submission1 = submissionBuild1.toString();
+
+            StringBuilder submissionBuild2 = new StringBuilder();
+            for (TokenInfo token : Submission2Tokens) {
+                submissionBuild2.append((char) token.type);
+            }
+            String submission2 = submissionBuild2.toString();
+
+            if (submission1.length() > submission2.length()) {
+                String temp = submission1;
+                submission1 = submission2;
+                submission2 = temp;
+
+            }
+
+            PlagResult res = GreedyStringTiling.run(submission1, submission2, 1, 0.8f);
+
+            result.setSimilarity((double) Math.round(res.getSimilarity()));
+
+            result.setSimilarityParts(res.getTiles().stream().map(t -> {
+                SimilarityPart part = new SimilarityPart();
+                part.setPatternPostion((long) t.patternPostion);
+                part.setLength((long) t.length);
+                part.setTextPosition((long) t.textPosition);
+                return part;
+            }).toList());
+
+            compareResult.getCompareFiles().add(result);
+        }
+
+
+
+        return compareResult;
+    }
 
 }
